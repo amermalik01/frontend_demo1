8d38209d0c3d072b311925e731a8a1eb81bc6ddb
Amer Mushtaq <amer.micromerger@gmail.com>
2025-04-29 20:56:05 +0500

    login form update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isSubmitted = false;
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOninit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submitLogin() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) return;
+
+    console.log(' form values ', this.fc['email'].value);
+    
+  }
 }
